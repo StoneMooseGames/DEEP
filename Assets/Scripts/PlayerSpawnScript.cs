1bad859c199a7b02f720707d4bb62e8de5409d8d
StoneMooseGames <mika.ahlsten@stonemoose.com>
2021-05-31 15:08:50 +0300

    modified spawner so that it randomly selects enemy from an array

--- Assets/Scripts/PlayerSpawnScript.cs
+++ Assets/Scripts/PlayerSpawnScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSpawnScript : MonoBehaviour
-{
+{ 
     GameObject player;
     // Start is called before the first frame update
     void Start()
